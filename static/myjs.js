3e6c63f37d833c99e9d4db867835d7719f459398
naibor <liznaibor.en@gmail.com>
2018-06-15 12:49:04 +0300

    [feature #158240400]add side nav ,request ride,table modals

--- static/myjs.js
+++ static/myjs.js
@@ -1,18 +1,16 @@
-var signupmodal = document.getElementById("SignUpModal");
-var loginmodal = document.getElementById("LogInModal");
-var signup = document.getElementById("SignUp");
-var login = document.getElementById("LogIn");
-var closesignup = document.getElementsByClassName("Close")[0];
-var closelogin =document.getElementsByClassName("Close")[1];
 // var menu =document.getElementById("menu");
-// var sidenav = document.getElementById("sidemodal");
 // var closesidenav = document.getElementsByClassName("Close");
+
+
 // signing up 
+var signup = document.getElementById("SignUp");
+var signupmodal = document.getElementById("SignUpModal");
 signup.onclick = function(){
     signupmodal.style.display="block";
     loginmodal.style.display="none";
 }
 // closing signup form modal
+var closesignup = document.getElementsByClassName("Close")[0];
 closesignup.onclick = function(){
     signupmodal.style.display="none";
 }
@@ -25,25 +23,43 @@ window.onclick = function(event){
 }
 
 // login modal form
+var login = document.getElementById("LogIn");
+var loginmodal = document.getElementById("LogInModal");
 login.onclick=function(){
     loginmodal.style.display="block";
     signupmodal.style.display="none";
 }
 // closing login form modal
+var closelogin =document.getElementsByClassName("Close")[1];
 closelogin.onclick=function(){
     loginmodal.style.display="none";
 }
 
 // menu button
-// menu.onclick=function(){
-//     sidenav.style.display="block";
+var sidenav = document.getElementById("sidemodal");
+function Open(){
+    sidenav.style.display="block";
+}
 
+// close side nav
+function Close(){
+    sidenav.style.display="none";
+}
 
+// request details
+var request = document.getElementById("requestDetails");
+function Open(){
+    request.style.display="block";
+}
+// close request details
+function Close(){
+    request.style.display="none";
+}
 
-// }
-// closing side nav
-// closesidenav.onclick=function(){
-//     sidenav.style.display="none";
-// }
+// request table
+var table =document.getElementsByClassName("requestTable");
+function Open(){
+    table.style.display="block";
+}
 
-
+// close table
