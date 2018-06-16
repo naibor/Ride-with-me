fd2806e788c9af8b5cabb8c7a979c41734f9e013
naibor <liznaibor.en@gmail.com>
2018-06-16 07:46:25 +0300

    [Feature finishes #158239763]fix request table modals

--- static/myjs.js
+++ static/myjs.js
@@ -1,48 +1,51 @@
-
-
-
 var signupmodal = document.getElementById("SignUpModal");
 var loginmodal = document.getElementById("LogInModal");
-var signup = document.getElementById("SignUp");
+// var signup = document.getElementById("SignUp");
 var login = document.getElementById("LogIn");
 var closesignup = document.getElementById("CloseSignUp");
 var closelogin =document.getElementById("CloseLogIn");
-var menu =document.getElementById("menu");
+// var menu =document.getElementById("menu");
 var sidenav = document.getElementById("sidemodal");
 var details = document.getElementById("requestDetails");
 var UserSideNav =document.getElementById("UsersideModal");
 var ridedetails = document.getElementById("requestRide");
+// tables
+var tablerequests = document.getElementById("requestTable")
 
 // signing up 
-signup.onclick = function(){
-    signupmodal.style.display="block";
-    loginmodal.style.display="none";
-}
+// signup.onclick = function(){
+//     signupmodal.style.display="block";
+//     loginmodal.style.display="none";
+// }
+// function OpenMenu(){
+
+// }
+
 // closing signup form modal
-closesignup.onclick = function(){
-    signupmodal.style.display="none";
-}
+// closesignup.onclick = function(){
+//     signupmodal.style.display="none";
+// }
 
 // when user clicks anywhere outside modal
-window.onclick = function(event){
-    if(event.target == signupmodal){
-        signupmodal.style.display="none";
-    }
-}
+// window.onclick = function(event){
+//     if(event.target == signupmodal){
+//         signupmodal.style.display="none";
+//     }
+// }
 
 // login modal form
-login.onclick=function(){
-    loginmodal.style.display="block";
-    signupmodal.style.display="none";
-}
+// login.onclick=function(){
+//     loginmodal.style.display="block";
+//     signupmodal.style.display="none";
+// }
 // closing login form modal
-closelogin.onclick=function(){
-    loginmodal.style.display="none";
-}
+// closelogin.onclick=function(){
+//     loginmodal.style.display="none";
+// }
 
 // menu button open side modal
 function OpenMenu(){
-    sidenav.style.display="block";
+    sidenav.style.display="flex";
     details.style.display="none";
 }
 // request details
@@ -70,11 +73,24 @@ function Openside(){
 function Closeside(){
     UserSideNav.style.display="none";
 }
+
+// open ride request
+function Openride(){
+    ridedetails.style.display="block";
+    UserSideNav.style.display="none";
+}
 // closing ride request
 function Closeride(){
     ridedetails.style.display="none";
 }
 
+// open table requests
 function Openrequests(){
-    ridedetails.style.display="block";   
+    tablerequests.style.display="block"
+}
+
+// close table requests
+function Closetable(){
+    tablerequests.style.display="none";
+    ridedetails.style.display="none";
 }
