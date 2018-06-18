bbbacbc2bfec11bf6dbec9ce2457d0c259bba553
naibor <liznaibor.en@gmail.com>
2018-06-18 17:04:02 +0300

    [chore #1584233389]set up flask,add environment configurations

---
+++ run.py
@@ -0,0 +1,9 @@
+from Api import app
+# flask instance
+
+from Instance.config import app_config
+# enviroment configuration settings
+
+if __name__=="__main__":
+    app.config.from_object(app_config['development'])
+    app.run()
