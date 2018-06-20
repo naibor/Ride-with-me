dee217a88fbe64c4a9a912b0176530a18b083fc3
naibor <liznaibor.en@gmail.com>
2018-06-20 17:09:40 +0300

    [Feature #158423117]refactor user resource

--- Api/User.py
+++ Api/User.py
@@ -1,22 +1,23 @@
 from flask_restful import Resource, Api
 # from flask import Flask
 from werkzeug.security import generate_password_hash, check_password_hash
-from datetime import datetime
 # hashes passwords
+from datetime import datetime
+# to get departure time
 
-# app = Flask('Api') 
-# flask instance
-# api = Api(app)
+
 signup_info = []
+# where user signup information is stored
 driver_info = []
+# where driver information is stored after registration
 driver_details = {}
+# the driver details a passanger user sees while viewing ride_offers[].
 
 class UserSignUp(Resource):
     # inherits from resource
 
     def get(self):
-        return {"okay":"this works"}
-
+        pass
 
     def post(self):
         # user post signup data
@@ -24,43 +25,42 @@ class UserSignUp(Resource):
         # returns data in json format
         hashed_password = generate_password_hash(signup_data.get("password"), method="sha256")
 
-        # validation starts now
-        # firstname
-        # lastname
-        # username
-        # password
-        # confirmpassword
+        # validation starts
 
         # checks if data field is empty
+        # firstname
         if not signup_data.get("firstname"):
-            return{'message':"enter first name"},401
+            return{'message':"enter first name"},400
+        # lastname
         if not signup_data.get("lastname"):
-            return{'message':"enter last name"},401
+            return{'message':"enter last name"},400
+        # username
         if not signup_data.get("username"):
-            return{'message':"enter user name"},401
+            return{'message':"enter user name"},400
+        # password
         if not signup_data.get("password"):
-            return{'message':"enter password"},401
+            return{'message':"enter password"},400
+        # confirmpassword
         if not signup_data.get("confirmpassword"):
-            return{'message':"enter confirm password"},401
+            return{'message':"enter confirm password"},400
 
-        # checks if data is an empty string
-
+        # checks if data is space filled 
         if signup_data.get("firstname") == " ":
-            return{'message':"enter visible character"},401
+            return{'message':"enter visible character"},400
         if signup_data.get("lastname") == " ":
-            return{'message':"enter visible character"},401
+            return{'message':"enter visible character"},400
         if signup_data.get("username") == " ":
-            return{'message':"enter visible character"},401
+            return{'message':"enter visible character"},400
         if signup_data.get("password") == " ":
-            return{'message':"enter visible character"},401
+            return{'message':"enter visible character"},400
         if signup_data.get("confirmpassword") == " ":
-            return{'message':"enter visible character"},401
+            return{'message':"enter visible character"},400
 
-        # confirms confirm password and password is the same
+        # confirms confirm password == password is the same
         if signup_data.get("password") != signup_data.get("confirmpassword"):
-            return{"message":"put same shit"}
+            return{"message":"password and confirm password not the same"}
         
-        # an instance of class ser containing user data
+        # an instance of User in models containing user data
         new_user = User(signup_data.get("firstname"),
                         signup_data.get("lastname"),
                         signup_data.get("username"),
@@ -68,7 +68,7 @@ class UserSignUp(Resource):
                         )
 
                         
-        # save new user to signup info a list
+        # save new user as a {}, to signup info[]
         signup_info.append({"firstname":new_user.firstname,
                       "lastname":new_user.lastname,
                       "username":new_user.username,
@@ -76,46 +76,46 @@ class UserSignUp(Resource):
                        })
         return{"message":"Welcome you have successfully signed up"},201            
  
- class DriverReg(Resource):
-     def post(self):
-        regData = request.get_json()
-        hashed_password = generate_password_hash(regData.get("password"), method="sha256")
+class DriverReg(Resource):
+    #class driver registration  resource
 
+    def post(self):regData = request.get_json()
+        hashed_password = generate_password_hash(regData.get("password"), method="sha256")
         # check empty field
         if not regData.get("firstname"):
-            return{'message':"enter first name"},401
+            return{'message':"enter first name"},400
         if not regData.get("lastname"):
-            return{'message':"enter last name"},401
+            return{'message':"enter last name"},400
         if not regData.get("username"):
-            return{'message':"enter user name"},401
+            return{'message':"enter user name"},400
         if not regData.get("phone_number"):
-            return{'message':"enter phone number"},401
+            return{'message':"enter phone number"},400
         if not regData.get("password"):
-            return{'message':"enter password"},401
+            return{'message':"enter password"},400
         if not regData.get("car"):
-            return{'message':"enter your type of car"},401
+            return{'message':"enter your type of car"},400
         if not regData.get("confirmpassword"):
-            return{'message':"enter confirm password"},401
+            return{'message':"enter confirm password"},400
 
         # checks for spaces only field
         if regData.get("firstname") == " ":
-            return{'message':"enter visible character"},401
+            return{'message':"enter visible character"},400
         if regData.get("lastname") == " ":
-            return{'message':"enter visible character"},401
+            return{'message':"enter visible character"},400
         if regData.get("username") == " ":
-            return{'message':"enter visible character"},401
+            return{'message':"enter visible character"},400
         if regData.get("phone_number") == " ":
-            return{'message':"enter visible character"},401
+            return{'message':"enter visible character"},400
         if regData.get("car") == " ":
-            return{'message':"enter visible character"},401    
+            return{'message':"enter visible character"},400    
         if regData.get("password") == " ":
-            return{'message':"enter visible character"},401
+            return{'message':"enter visible character"},400
         if regData.get("confirmpassword") == " ":
-            return{'message':"enter visible character"},401
+            return{'message':"enter visible character"},400
 
-     # confirms confirm password and password is the same
+        #confirms confirmpassword == password is the same
         if regData.get("password") != regData.get("confirmpassword"):
-            return{"message":"password and confirm password not the same"}
+            return{"message":"password and confirm password not the same"},400
         
         # an instance of class ser containing user data
         new_driver = Driver(regData.get("firstname"),
@@ -124,18 +124,18 @@ class UserSignUp(Resource):
                             regData.get("car"),
                             hashed_password
                             )
-        # save driver_details a dict the one people will see
+        # save driver_details to driver_detail{} will be see by passangers
         driver_details[new_driver.firstname] = {"type_of_car":new_driver.car}
 
                         
-        # save new drive to driver info a list
+        # save new drive to driver info[]
         driver_info.append({"firstname":new_user.firstname,
                             "lastname":new_user.lastname,
                             "username":new_user.username,
                             "password":hashed_password,
                             "driver_detail":driver_details
                             })
-        return{"message":"Welcome you have successfully registeredto be a driver"},201  
+        return{"message":"Welcome you have successfully registered as a driver"},201  
 
 
 <<<<<<< HEAD
--- models/ride_models.py
+++ models/ride_models.py
@@ -3,16 +3,9 @@ from datetime import datetime, timedelta
 request_ride={
     "location":"nakuru",
     "destination":"kakamega",
-<<<<<<< HEAD
-}
-
-ride_offers = []
-
-=======
 
 }
->>>>>>> ft-get-ride-offers-api-158423117
-driver_info={}
+driver_info=[]
 driver_detail={
     "name":"kamau",
     "car":"toyota"
