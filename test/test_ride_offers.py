6752576cfa6e2aa6c72b00e49e3f4048d9773fb0
naibor <liznaibor.en@gmail.com>
2018-06-21 14:13:51 +0300

    [Feature finishes #158430273]have passing login and signup tests

--- test/test_ride_offers.py
+++ test/test_ride_offers.py
@@ -1,39 +1,43 @@
-import unittest
-import os
-import json
-from datetime import datetime, timedelta 
-from Api.models import ride_models
+# import unittest
+# import os
+# import json
+# from datetime import datetime, timedelta 
+# from models import ride_models
 
-from Api.resource import create_app
+# from Api import create_app
  
-class TestRideOffers(unittest.TestCase):
-    def setUp(self):
-        """inititalize app and define variables"""
-        self.app = create_app(config_name = "testing")
-        # allows our test to mimic actual clients
-        self.client = self.app.test_client()
-        # departure time 20 minutes from current time 
-        DTime = datetime.now() + timedelta(minutes=20)
-        self.sign_up =self.client.post(
-            "api/v1/users",
-            data = json.dumps(dict(
-                RideId= "1",
-                location = "Nanyuki",
-                destination = "kisumu",
-                departure = str(DTime.time()),
-                driver_details = (dict(
-                    name = "kamau",
-                    car = "toyoya"
-                ))
-            )),
-            content_type = "application/json"
-        )   
+# class TestRideOffers(unittest.TestCase):
+#     def setUp(self):
+#         """inititalize app and define variables"""
+#         self.app = create_app(config_name = "testing")
+#         # allows our test to mimic actual clients
+#         self.app.config["TESTING"]=True
+#         self.client = self.app.test_client()
+#         # departure time 20 minutes from current time 
+#         DTime = datetime.now() + timedelta(minutes=20)
+#         self.sign_up =self.client.post(
+#             "api/v1/users",
+#             data=json.dumps(dict(
+#                 RideId= "1",
+#                 location = "Nanyuki",
+#                 destination = "kisumu",
+#                 departure = str(DTime.time()),
+#                 driver_details = (dict(
+#                     name = "kamau",
+#                     car = "toyoya"
+#                 ))
+#             )),
+#             content_type = "application/json"
+#         )  
+#     def test_create_ride_offers(self):
+#         """test driver can create ride offer"""
+#         self.assertEqual(response.status_code,201) 
         
-    def test_get_ride_offers(self):
-        """test user can view all ride offers"""
-        response = self.client.get("api/v1/ride",content_type = "application/json")
-        self.assertEqual(response.status_code,200 )
+#     def test_get_ride_offers(self):
+#         """test user can view all ride offers"""
+#         response = self.client.get("api/v1/ride",content_type = "application/json")
+#         self.assertEqual(response.status_code,200 )
 
-    def tearDown(self):
-        pass
+#     def tearDown(self):
+#         pass
 
---
+++ test/test_user.py
@@ -0,0 +1,74 @@
+import unittest  
+import os
+import json
+from flask import Flask
+
+# from Api.resource import create_app
+from Api import User
+from Api import create_app
+
+class TestUserSignUp(unittest.TestCase):
+    """class for user sign up test case"""
+    def setUp(self):
+        """initialize app and define variables"""
+        self.app = create_app(config_name = "testing")
+        # self.app = Flask(__name__)
+        # or the below whenyou are not using create_app funtion
+        self.app.config["TESTING"]=True
+        # a flask environmnet variable 
+        self.client = self.app.test_client()
+
+
+        # sign up a user
+    def test_user_sign_up(self):
+        """test user can successfuly sign up"""
+        response =self.sign_up =self.client.post(
+            "api/v1/user/signup",
+            data=json.dumps(dict(
+                firstname="Naibor",
+                lastname="Leona",
+                username="Lisa",
+                password="12345",
+                confirmpassword="12345"
+            )),
+            content_type=("application/json")
+        )
+        # assert response code is 201
+        self.assertEqual(response.status_code,201)
+        # deserialize response data
+        response_data = json.loads(response.data.decode())
+
+        self.assertEqual(response_data["message"],"Welcome you have successfully signed up")
+    
+    def test_user_login(self):
+        """test user can successfuly login"""
+        signup =self.sign_up =self.client.post(
+            "api/v1/user/signup",
+            data=json.dumps(dict(
+                firstname='Naib',
+                lastname='Leon',
+                username='Lis',
+                password='12345',
+                confirmpassword='12345'
+            )),
+            content_type=("application/json")
+        )
+
+        login = self.client.post(
+            "api/v1/user/auth",
+            data = json.dumps(dict(
+                username = "Lis",
+                password = "12345"
+            )),
+            content_type = "application/json"
+        )
+        # self.assertEqual(response.)
+        self.assertEqual(login.status_code,200)
+        response = json.loads(login.data.decode())
+        self.assertEqual(response["message"],"successfully logged in")
+
+        
+
+
+    def tearDown(self):
+        pass
