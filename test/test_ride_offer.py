e239e0ecb8942494f109614da6ecf5b39fbedc8a
naibor <liznaibor.en@gmail.com>
2018-06-27 17:12:15 +0300

    [Feature #158633235]add ride offer tests

--- test/test_ride_offer.py
+++ test/test_ride_offer.py
@@ -22,91 +22,59 @@ class TestRideOffer(unittest.TestCase):
         create = self.client.post(
             "/api/v1/users/rides",
             data = json.dumps(dict(
-                # RideId = "1",
-                # location = "Nanyuki",
-                # destination = "kisumu",
-                # departure = str(self.DTime.time())
-                # driver_details = (dict(
-                #     name = "kamau",
-                #     car = "toyoya"
-                # ))
+                RideId = "1",
+                location = "Nanyuki",
+                destination = "kisumu",
+                departure = str(self.DTime.time())
             )),
             headers = {"content-type": "application/json"}
         )  
 
-        # self.assertEqual(response.status_code,201) 
-        # response = self.client.get('/api/v1/user/offer/<location>',
-        #             headers = {"content-type": "application/json"})
-        self.assertEqual(create.status_code,200 )
+        self.assertEqual(create.status_code,201) 
+        response = self.client.get(
+            '/api/v1/users/rides',
+            headers = {"content-type": "application/json"}
+        )
+        self.assertEqual(response.status_code,200 )
 
-    # def test_empty_offer_location(self):
-    #     """tests user inputs location and destination"""
-    #     response = self.client.post(
-    #         "api/v1/user/create",
-    #         data = json.dumps(dict(
-    #             RideId = "1",
-    #             location = "",
-    #             destination = "kisumu",
-    #             departure = str(self.DTime.time()),
-    #             driver_details = (dict(
-    #                 name = "kamau",
-    #                 car = "toyoya"
-    #             ))
-    #         )),
-    #         headers = {"content-type": "application/json"}
-    #     ) 
-    #     self.assertEqual(response.status_code,400)  
+    def test_empty_offer_location(self):
+        """tests user inputs location and destination"""
+        response = self.client.post(
+            "api/v1/users/rides",
+            data = json.dumps(dict(
+                RideId = "1",
+                location = "",
+                destination = "kisumu",
+                departure = str(self.DTime.time())
+            )),
+            headers = {"content-type": "application/json"}
+            ) 
+        self.assertEqual(response.status_code,400)  
         
-    # def test_empty_offers_destination(self):
-    #     """test user can view all ride offers"""
-    #     response = self.client.post(
-    #         "api/v1/user/create",
-    #         data = json.dumps(dict(
-    #             RideId = "1",
-    #             location = "Nanyuki",
-    #             destination = "",
-    #             departure = str(self.DTime.time()),
-    #             driver_details = (dict(
-    #                 name = "kamau",
-    #                 car = "toyoya"
-    #             ))
-    #         )),
-    #         headers = {"content-type": "application/json"}
-    #     )  
-    #     self.assertEqual(response.status_code,400) 
+    def test_empty_offers_destination(self):
+        """test user can view all ride offers"""
+        response = self.client.post(
+            "api/v1/users/rides",
+            data = json.dumps(dict(
+                RideId = "1",
+                location = "Nanyuki",
+                destination = "",
+                departure = str(self.DTime.time())
+            )),
+            headers = {"content-type": "application/json"}
+        )  
+        self.assertEqual(response.status_code,400) 
 
-    # def tests_spaces_only_offer_location(self):
-    #     """tests user inputs valid characters only"""
-    #     response = self.client.post(
-    #         "api/v1/user/create",
-    #         data = json.dumps(dict(
-    #             RideId = "1",
-    #             location = "     ",
-    #             destination = "kisumu",
-    #             departure = str(self.DTime.time()),
-    #             driver_details = (dict(
-    #                 name = "kamau",
-    #                 car = "toyoya"
-    #             ))
-    #         )),
-    #         headers = {"content-type": "application/json"}
-    #     ) 
-    #     self.assertEqual(response.status_code,400)  
-    
-    # def test_get_ride_offers(self):
-    #     """test user can view all ride offers"""
-    #     response = self.client.post(
-    #         "api/v1/user/create",
-    #         data = json.dumps(dict(
-    #             RideId = "1",
-    #             location = "Nanyuki",
-    #             destination = "kisumu",
-    #             departure = str(self.DTime.time()),
-    #             driver_details = (dict(
-    #                 name = "kamau",
-    #                 car = "toyoya"
-    #             ))
-    #         )),
-    #         headers = {"content-type": "application/json"}
-    #     )  
-    #     self.assertEqual(response.status_code,201) 
+    def tests_spaces_only_offer_location(self):
+        """tests user inputs valid characters only"""
+        response = self.client.post(
+            "api/v1/users/rides",
+            data = json.dumps(dict(
+                RideId = "1",
+                location = "   ",
+                destination = "kisumu",
+                departure = str(self.DTime.time())
+            )),
+            headers = {"content-type": "application/json"}
+        ) 
+        self.assertEqual(response.status_code,400)  
