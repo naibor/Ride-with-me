c76777ff280f1ec30bf6a43d7dddebb42e6b7817
naibor <liznaibor.en@gmail.com>
2018-07-13 14:50:16 +0300

    [chore #158894615]update travis.yml file

--- .travis.sh
+++ .travis.sh
@@ -1,3 +1,3 @@
-export DATABASE_NAME="testdb"
-export DATABASE_USER="naibor"
-export DATABASE_PASSWORD="lisanaibor"
+export DATABASE_NAME='testdb'
+export DATABASE_USER='naibor'
+export DATABASE_PASSWORD='lisanaibor'
