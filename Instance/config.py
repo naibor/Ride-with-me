2ad37d1df1ba0ef49460d7803e35dda97aee3c22
naibor <liznaibor.en@gmail.com>
2018-07-22 19:56:40 +0300

    [Chore #158625517]update config

--- Instance/config.py
+++ Instance/config.py
@@ -11,21 +11,18 @@ class Config(object):
 class DevelopmentConfig(Config):
     """configurations for development"""
     DEBUG = True
-    DATABASE = os.getenv('.env')
 
 
 class TestingConfig(Config):
     """configurations for testing """
     TESTING = True
     DEBUG = True
-    DATABASE = os.getenv('.env.testing')
 
 
 class ProductionConfig(Config):
     """Configuration for production"""
     DEBUG = True
     TESTING = False
-    DATABASE = os.getenv('.env')
 
 app_config = {
     'development':DevelopmentConfig,
